0b8d2722898f159ac256c9ea0a45cff6f75cea72
AndersonExner <andersonexner@gmail.com>
2023-12-25 11:59:35 -0300

    feature: insert user dto

---
+++ src/app/features/users/dto/user.dto.ts
@@ -0,0 +1,12 @@
+export interface UserDetailDTO {
+    id: string;
+    name: string;
+    email: string;
+}
+
+export interface NewUserDTO {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+}
